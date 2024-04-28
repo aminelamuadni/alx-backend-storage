6b1f4f9782be6bcad61735d5e97143149e80d7ab
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-28 07:10:40 +0100

    Update 5-valid_email.sql file

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,12 @@
--- Drop existing trigger if it exists
+-- Drop existing trigger if it exists to avoid conflicts
 DROP TRIGGER IF EXISTS resets_valid_email;
 
--- Create a new trigger to reset the valid_email field when the email changes
+-- Create a trigger to reset the valid_email field on email update
 DELIMITER $ $ CREATE TRIGGER resets_valid_email BEFORE
 UPDATE
     ON users FOR EACH ROW BEGIN IF NEW.email != OLD.email THEN
 SET
-    NEW.valid_email = FALSE;
+    NEW.valid_email = 0;
 
 END IF;
 
