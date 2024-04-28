7a0cb691d61e066fb943ed3f0b78f2250d376e94
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-28 07:25:30 +0100

    Update 8-index_my_names.sql file

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,2 @@
--- Drop the index if it exists to avoid duplication errors
-DROP INDEX IF EXISTS idx_name_first ON names;
-
 -- Create an index on the first letter of the 'name' column
 CREATE INDEX idx_name_first ON names(name(1));
