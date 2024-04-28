691cb34318599091ff8638f982d128242c0e1be2
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-28 07:24:58 +0100

    Update 8-index_my_names.sql file

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,11 +1,5 @@
--- Drop the index if it exists (in case of re-running the script)
+-- Drop the index if it exists to avoid duplication errors
 DROP INDEX IF EXISTS idx_name_first ON names;
 
--- Add a generated column to store the first letter of name
-ALTER TABLE
-    names
-ADD
-    COLUMN first_letter CHAR(1) AS (SUBSTRING(name, 1, 1)) STORED;
-
--- Create an index on the generated column
-CREATE INDEX idx_name_first ON names(first_letter);
+-- Create an index on the first letter of the 'name' column
+CREATE INDEX idx_name_first ON names(name(1));
