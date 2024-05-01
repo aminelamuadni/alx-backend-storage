8c005066b763f44ac046372c6498759da0950c8a
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-01 06:30:14 +0100

    Add 102-log_stats.py file

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Module to provide statistics about Nginx logs stored in MongoDB with top IP
+stats.
+
+This script connects to the MongoDB, accesses the nginx collection within the
+logs database, and displays various statistics including the number of logs,
+method usage counts, count of GET requests to '/status', and the top 10 most
+frequent IP addresses.
+"""
+
+from pymongo import MongoClient
+
+
+def log_stats(nginx_collection):
+    """
+    Prints enhanced stats about Nginx logs stored in MongoDB, including top 10
+    IPs.
+
+    Args:
+        nginx_collection (Collection): A PyMongo collection object from the
+        'nginx' collection.
+
+    Outputs:
+        Prints total log count, counts by HTTP method, count of GET requests to
+        '/status', and the top 10 most frequent IP addresses.
+    """
+    # Total logs count
+    logs_count = nginx_collection.estimated_document_count()
+    print(f"{logs_count} logs")
+
+    # Counts by method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Count of GET requests to '/status'
+    status_count = nginx_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
+    print(f"{status_count} status check")
+
+    # Top 10 IPs
+    ip_pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = list(nginx_collection.aggregate(ip_pipeline))
+    print("IPs:")
+    for ip in top_ips:
+        print(f"    {ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    # Connect to the MongoDB instance
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    # Execute the statistics logging function
+    log_stats(nginx_collection)
