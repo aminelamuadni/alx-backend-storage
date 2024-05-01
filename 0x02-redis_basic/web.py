d3f1a116ed48737b242848da929bbea844271a1f
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-01 07:41:56 +0100

    Update web.py file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
-""" Module to implement an expiring web cache and tracker.
-The function fetches HTML content from URLs, tracks the number of accesses
-using Redis, and caches the results with a 10-second expiration time.
-"""
+""" Module to implement an expiring web cache and tracker. """
 
-import requests
 import redis
-from functools import wraps
+import requests
 
-redis_client = redis.Redis(decode_responses=True)
-
-
-def count_and_cache_requests(func):
-    """Decorator to count URL requests and cache HTML content."""
-    @wraps(func)
-    def wrapper(url):
-        count_key = f"count:{url}"
-        cache_key = f"cached:{url}"
-
-        cached_content = redis_client.get(cache_key)
-        if cached_content:
-            return cached_content
-
-        html_content = func(url)
-
-        redis_client.incr(count_key)
-        redis_client.setex(cache_key, 10, html_content)
-
-        return html_content
-    return wrapper
+redis_client = redis.Redis()
 
 
-@count_and_cache_requests
 def get_page(url: str) -> str:
-    """Fetch HTML content from a specified URL."""
+    """
+    Fetches the HTML content of a specified URL and caches it. It also tracks
+    how many times the URL has been accessed.
+
+    Args:
+    url (str): URL to fetch the HTML content from.
+
+    Returns:
+    str: HTML content of the page.
+    """
+    count_key = f"count:{url}"
+    redis_client.incr(count_key)
+
+    cache_key = f"cached:{url}"
+    cached_content = redis_client.get(cache_key)
+    if cached_content:
+        return cached_content.decode('utf-8')
+
     response = requests.get(url)
-    return response.text
+    html_content = response.text
+
+    redis_client.setex(cache_key, 10, html_content)
+
+    return html_content
+
+
+if __name__ == "__main__":
+    test_url = 'http://slowwly.robertomurray.co.uk/delay/5000/url/' \
+               'http://www.google.com'
+    print(get_page(test_url))
+    print(get_page(test_url))
